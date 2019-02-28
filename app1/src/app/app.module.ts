325df078e620305747ea41f355e62083c39ffd13
Rafael Oliveira <rafaelntn766@gmail.com>
2019-02-28 15:45:46 -0300

    Starting first project

---
+++ app1/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {TopoComponent} from './topo/topo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app1/src/app/topo/topo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-topo', 
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
+
+    // styles : [ .exemplo { color: blue } ] -----css inline
+
+    // template: `<p>
+    //         Testing my first component -----template inline
+    //     </p>`
+
+})
+export class TopoComponent {}
