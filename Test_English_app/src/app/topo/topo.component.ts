aaaff6ff9217438c11c5e492b62b9a80df28fdeb
Rafael Oliveira <rafaelntn766@gmail.com>
2019-03-01 11:06:42 -0300

    Implementing HTML and CSS

--- Test_English_app/src/app/topo/topo.component.ts
+++ Test_English_app/src/app/topo/topo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: 'app-topo', 
+    selector: '.app-topo', 
     templateUrl: './topo.component.html',
     styleUrls: ['./topo.component.css']
 
