9e6bea2fed022c7550ab14ee70f27811a4fd5752
Rafael Oliveira <rafaelntn766@gmail.com>
2019-03-04 14:56:47 -0300

    Aprimoring the app

--- Test_English_app/src/app/painel/painel.component.ts
+++ Test_English_app/src/app/painel/painel.component.ts
@@ -11,11 +11,59 @@ import { FRASES } from './frases-mock'
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
-  public instrucao : string = 'Traduza a frase'
+  public instrucao: string = 'Traduza a frase'
+  public resposta: string = ''
 
-  constructor() {  }
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+
+  public tentativas: number = 3
+
+  constructor() {
+    this.atualizaRodada()
+  }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta')
+
+      //trocar pergunta da rodada
+      this.rodada++
+
+      //progresso
+      this.progresso = this.progresso + 25 //(100 / this.frases.length)
+
+      //atualiza o objeto rodadaFrase
+      this.atualizaRodada()
+
+    } else {
+      //diminuir tentativas
+      this.tentativas--
+
+      if(this.tentativas === -1){
+        alert('Você perdeu todas as tentativas')
+      }
+    }
+
+  }
+
+  public atualizaRodada(): void {
+
+    //define a frase da rodada com base em alguma lógica
+    this.rodadaFrase = this.frases[this.rodada]
+
+    //limpar resposta
+    this.resposta = ''
   }
 
 }
